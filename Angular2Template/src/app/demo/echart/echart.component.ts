573784f4a1654ec87fc7e406c34cc44f23cf9a26
zhangzhengyi <zhangtaihu101@163.com>
2020-01-20 15:57:45 +0800

    echarts

--- Angular2Template/src/app/demo/echart/echart.component.ts
+++ Angular2Template/src/app/demo/echart/echart.component.ts
@@ -11,7 +11,7 @@ export class EChartComponent implements OnInit {
 
   chartOption = {
     title: {
-      text: 'ECharts实例',
+      text: '柱状图（bar）',
       textStyle: {
         color: '#666666',
         fontSize: 16
@@ -28,19 +28,59 @@ export class EChartComponent implements OnInit {
       }]
     },
     grid: {
-
+      x: '10%', y: '10%', width: '80%', height: '80%'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
     },
     xAxis: {
       type:'category',
+      axisLine: {
+        lineStyle: {
+          color: '#3bafda'
+        }
+      },
+      axisTick: {
+        lineStyle: {
+          color: '#3bafda'
+        }
+      },
+      axisLabel: {
+        color: '#666666'
+      },
+      splitLine: {
+      },
       data: ['One','Two','Three','Four','Five']
     },
     yAxis: {
-      type: 'value'
+      type: 'value',
+      axisLine: {
+        lineStyle: {
+          color: '#3bafda'
+        }
+      },
+      axisTick: {
+        lineStyle: {
+          color: '#3bafda'
+        }
+      },
+      axisLabel: {
+        color: '#666666'
+      },
+      splitLine: {
+        lineStyle: {
+          color: '#eeeeee'
+        }
+      },
     },
     series: [{
       name: 'Name',
       type: 'bar',
       barWidth: '60%',
+      color: ['#3bafda'],
       data: [90,50,100,20,80]
     }]
   };
